45f4476c8899ec5ea7be73576838505ec79be13a
shuitest <shuitest@gmail.com>
2017-03-09 10:00:05 +1000

    Create index.php

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+ini_set('display_errors', true);
+ini_set('error_reporting', E_ALL&~E_NOTICE);
+ini_set('log_errors', true);
+ini_set('display_startup_errors', true);
+
+ini_set('error_log', dirname(__FILE__).'/error_log');
